d8b89c89739e90de796a4ed7353290c52760584f
SuperKXT <superkxt@outlook.com>
2023-05-24 18:37:39 +0500

    Update `config`

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,13 +1,20 @@
-export const environment = window.getIsPackaged()
-	? 'production'
-	: 'development';
+export const getEnvironment = async () => {
+	const isPackaged = await window.ipc.app.getIsPackaged();
+	return isPackaged ? 'production' : 'development';
+};
 
-export const apiEndpoint = {
-	development: 'http://10.0.0.9:8888/api',
-	production: 'http://172.16.1.83:4005/api',
-}[environment];
+export const getApiEndpoint = async () => {
+	const environment = await getEnvironment();
+	return {
+		development: 'http://10.0.0.9:8888/api',
+		production: 'http://172.16.1.83:4005/api',
+	}[environment];
+};
 
-export const isFetchMocked = {
-	development: false,
-	production: false,
-}[environment];
+export const getIsFetchMocked = async () => {
+	const environment = await getEnvironment();
+	return {
+		development: true,
+		production: false,
+	}[environment];
+};
