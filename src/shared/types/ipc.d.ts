d6638d94fcbabb576c37ef5ac651d372fddbb19d
SuperKXT <superkxt@outlook.com>
2023-05-24 17:11:51 +0500

    Tweak typescript config

--- src/shared/types/ipc.d.ts
+++ src/shared/types/ipc.d.ts
@@ -1,4 +1,7 @@
-import type { IpcMethods } from '~/electron/ipc';
+export type IpcMethods = {
+	getIsPackaged: () => boolean;
+	closeApplication: () => void;
+};
 
 declare global {
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-interface
