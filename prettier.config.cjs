1f0892a3aed65ce14d62b96d5e924550440005ae
SuperKXT <superkxt@outlook.com>
2023-05-24 13:34:19 +0500

    Add `prettier` config and ignore file

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	singleAttributePerLine: true,
+};
+
+// eslint-disable-next-line import/no-commonjs
+module.exports = config;
