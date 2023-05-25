e8d9dffe7b1502460a0408c0b68c0b055a8f6d5d
SuperKXT <superkxt@outlook.com>
2023-05-25 17:09:50 +0500

    Fix config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,17 +1,19 @@
-const environment = process.env.DEV ? 'development' : 'production';
-console.log(process.env.DEV);
+export const environment = window.ipc.app.environment;
 
 export const apiEndpoint = {
 	development: 'http://10.0.0.9:8888/api',
+	test: 'http://10.0.0.9:8888/api',
 	production: 'http://172.16.1.83:4005/api',
 }[environment];
 
 export const isFetchMocked = {
 	development: false,
+	test: false,
 	production: false,
 }[environment];
 
 export const disableAuth = {
 	development: false,
+	test: false,
 	production: false,
 }[environment];
