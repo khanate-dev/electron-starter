bde364107fe9952744740280804a0b5388aeef30
SuperKXT <superkxt@outlook.com>
2023-05-25 18:22:18 +0500

    Update `user.context` for route changes

--- src/app/contexts/user/user.context.tsx
+++ src/app/contexts/user/user.context.tsx
@@ -1,46 +1,29 @@
 import { createContext, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import {
-	getSetting,
-	removeSetting,
-	setSetting,
-} from '~/shared/helpers/settings';
-import { dayjsUtc } from '~/shared/helpers/date';
+import { removeSetting } from '~/shared/helpers/settings';
 
 import type { LoggedInUser } from '~/app/schemas/user';
 
 const UserContext = createContext<null | LoggedInUser>(null);
 
-const defaultUser: LoggedInUser = {
-	userID: 1 as DbId,
-	userName: 'admin',
-	userType: 'Administrator',
-	token: '8224f044-0407-4021-aba9-aa537354ca43',
-	imageUpdatedAt: null,
-	createdAt: dayjsUtc.utc(),
-	updatedAt: dayjsUtc.utc(),
+type UserProviderProps = ComponentWithChildren & {
+	user: LoggedInUser;
 };
 
-export const UserProvider = ({ children }: ComponentWithChildren) => {
+export const UserProvider = ({ user, children }: UserProviderProps) => {
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		if (!getSetting('user')) setSetting('user', defaultUser);
-
 		const logout = () => {
-			if (!getSetting('user')) setSetting('user', defaultUser);
 			removeSetting('user');
 			navigate('/login');
 		};
 		window.addEventListener('logout', logout);
-
 		return () => window.removeEventListener('logout', logout);
 	}, [navigate]);
 
-	return (
-		<UserContext.Provider value={defaultUser}>{children}</UserContext.Provider>
-	);
+	return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 };
 
 /** fires the logout event to trigger a logout and redirect to login screen */
