45013c83355d6f7aee7f7639d9ab63568ce96090
SuperKXT <superkxt@outlook.com>
2023-05-25 16:17:12 +0500

    Fix `webpack.main.config`

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -9,7 +9,7 @@ export const mainConfig: Configuration = {
 	 * This is the main entry point for your application, it's the first file
 	 * that runs in the main process.
 	 */
-	entry: './src/main/main.ts',
+	entry: './src/electron/main.ts',
 	// Put your normal webpack config below here
 	module: { rules },
 	resolve: {
