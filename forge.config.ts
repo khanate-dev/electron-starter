219f1fad3e77c8f1162ed2d0077db223ecf4b893
SuperKXT <superkxt@outlook.com>
2023-05-25 16:14:54 +0500

    Update `forge.config`

--- forge.config.ts
+++ forge.config.ts
@@ -9,28 +9,83 @@ import { rendererConfig } from './webpack.renderer.config';
 
 import type { ForgeConfig } from '@electron-forge/shared-types';
 
+const id = 'cbl-garment-tracking';
+const name = 'CBL Garment Tracking';
+const description = 'Garment Tracking Desktop Application For CBL';
+const icoIcon = './src/app/assets/favicon/icon.ico';
+const pngIcon = './src/app/assets/favicon.png';
+const repository = {
+	owner: 'WiMetrixDev',
+	name: 'cbl-garment-tracking',
+};
+
 const config: ForgeConfig = {
-  packagerConfig: {},
-  rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
-  plugins: [
-    new WebpackPlugin({
-      mainConfig,
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
-            preload: {
-              js: './src/preload.ts',
-            },
-          },
-        ],
-      },
-    }),
-  ],
+	packagerConfig: {
+		icon: icoIcon,
+		name: id,
+		executableName: id,
+	},
+	rebuildConfig: {},
+	makers: [
+		new MakerSquirrel({
+			name: id,
+			description,
+			exe: name,
+			setupExe: id,
+			setupIcon: icoIcon,
+		}),
+		new MakerZIP({}, ['darwin']),
+		new MakerRpm({
+			options: {
+				name: id,
+				icon: pngIcon,
+				categories: ['Office', 'Utility'],
+				genericName: name,
+				description,
+				productName: name,
+				productDescription: description,
+			},
+		}),
+		new MakerDeb({
+			options: {
+				name: id,
+				icon: pngIcon,
+				categories: ['Office', 'Utility'],
+				genericName: name,
+				description,
+				productName: name,
+				productDescription: description,
+				section: 'javascript',
+			},
+		}),
+	],
+	publishers: [
+		{
+			name: '@electron-forge/publisher-github',
+			config: {
+				repository,
+			},
+		},
+	],
+	plugins: [
+		new WebpackPlugin({
+			mainConfig,
+			renderer: {
+				config: rendererConfig,
+				entryPoints: [
+					{
+						html: './src/app/index.html',
+						js: './src/app/index.tsx',
+						name: 'main_window',
+						preload: {
+							js: './src/electron/preload.ts',
+						},
+					},
+				],
+			},
+			devContentSecurityPolicy: `default-src 'self' 'unsafe-inline' data:; script-src 'self'; connect-src http: https: ws:`,
+		}),
+	],
 };
 
 // eslint-disable-next-line import/no-default-export
