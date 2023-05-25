830dac5c3b9b65204661f961c4042dce9e66e6d8
SuperKXT <superkxt@outlook.com>
2023-05-25 17:10:37 +0500

    Add serial port ipc handlers back

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,11 @@
+import { environment } from './config';
+
 export const App = () => {
 	return (
 		<>
 			<h1>💖 Hello World! 💖</h1>
 			<p>Welcome to your Electron application.</p>
+			<p>Running in {environment}</p>
 		</>
 	);
 };
--- src/electron/ipc.ts
+++ src/electron/ipc.ts
@@ -1,5 +1,7 @@
 import { app, ipcMain } from 'electron';
 
+import { setupReader } from './serial-port';
+
 import type { BrowserWindow } from 'electron';
 import type { IpcApi } from '~/shared/types/ipc';
 
@@ -7,5 +9,5 @@ export const setupIpc = (mainWindow: BrowserWindow) => {
 	ipcMain.on('closeApplication', (() =>
 		app.exit()) satisfies IpcApi['app']['closeApplication']);
 
-	// setupReader(mainWindow);
+	setupReader(mainWindow);
 };
