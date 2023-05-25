a33bbbc73b0d7b2488eab34536857a8250fe016c
SuperKXT <superkxt@outlook.com>
2023-05-25 18:23:07 +0500

    Setup routing in `app.tsx`

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,38 @@
-import { environment } from './config';
+import { createHashRouter } from 'react-router-dom';
+
+import { Dashboard, dashboardRoutes } from '~/app/routes/dashboard';
+import { Welcome } from '~/app/routes/dashboard/welcome';
+import { Providers } from '~/app/components/app/providers';
+import { ErrorBoundary } from '~/app/components/app/error-boundary';
+import { Login } from '~/app/routes/login';
+
+const router = createHashRouter([
+	{
+		path: 'login',
+		element: <Login />,
+		errorElement: <ErrorBoundary />,
+		loader: Login.loader,
+	},
+	{
+		path: '',
+		element: <Dashboard />,
+		errorElement: <ErrorBoundary />,
+		loader: Dashboard.loader,
+		children: [
+			{
+				errorElement: <ErrorBoundary />,
+				children: [
+					{
+						index: true,
+						element: <Welcome />,
+					},
+					...dashboardRoutes,
+				],
+			},
+		],
+	},
+]);
 
 export const App = () => {
-	return (
-		<>
-			<h1>💖 Hello World! 💖</h1>
-			<p>Welcome to your Electron application.</p>
-			<p>Running in {environment}</p>
-		</>
-	);
+	return <Providers router={router} />;
 };
