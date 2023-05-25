dc213ecad67a21b8aefd20acd6a2724ffd0a2162
SuperKXT <superkxt@outlook.com>
2023-05-25 18:22:03 +0500

    Add `login` route

---
+++ src/app/routes/login/login.route.tsx
@@ -0,0 +1,163 @@
+import { redirect, useNavigate } from 'react-router-dom';
+import {
+	Typography,
+	Paper,
+	Stack,
+	AppBar,
+	Toolbar,
+	alpha,
+} from '@mui/material';
+
+import { FormSchema } from '~/app/schemas';
+import { userZodSchema } from '~/app/schemas/user';
+import { login } from '~/app/endpoints/user';
+import { getSetting, setSetting } from '~/shared/helpers/settings';
+import { WiMetrixLogo } from '~/app/components/media/wimetrix-logo';
+import { BackgroundImage } from '~/app/components/media/background-image';
+import { SchemaForm } from '~/app/components/forms/schema-form';
+import { AppLogo } from '~/app/components/media/app-logo';
+import { ThemeSwitch } from '~/app/components/controls/theme-switch';
+import { LOGIN_HEADER_HEIGHT } from '~/app/config';
+
+const loginSchema = new FormSchema({
+	name: 'login',
+	zod: userZodSchema.pick({ userName: true, password: true }),
+	fields: {
+		userName: { type: 'string' },
+		password: { type: 'string' },
+	},
+});
+
+const headerLogoSx = {
+	width: 'auto',
+	height: 'auto',
+	maxWidth: 300,
+	maxHeight: '70%',
+};
+
+const loader = () => {
+	const user = getSetting('user');
+	if (user) return redirect('/');
+	return null;
+};
+
+export const Login = () => {
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<AppBar
+				position='fixed'
+				elevation={0}
+				sx={{
+					zIndex: (theme) => theme.zIndex.drawer + 1,
+					transition: (theme) =>
+						theme.transitions.create(['width', 'margin'], {
+							easing: theme.transitions.easing.sharp,
+							duration: theme.transitions.duration.leavingScreen,
+						}),
+					background: (theme) => `linear-gradient(
+						to left,
+						${alpha(theme.palette.secondary[theme.palette.mode], 0.3)},
+						${alpha(theme.palette.primary[theme.palette.mode], 0.3)}
+					)`,
+					height: LOGIN_HEADER_HEIGHT,
+					display: 'flex',
+					alignItems: 'stretch',
+					boxShadow: 'unset',
+				}}
+			>
+				<Stack
+					direction='row'
+					component={Toolbar}
+					sx={{
+						height: '100%',
+						padding: '0 2em',
+						minHeight: '100%',
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+					}}
+					disableGutters
+				>
+					<AppLogo sx={headerLogoSx} />
+					<ThemeSwitch />
+					<WiMetrixLogo sx={headerLogoSx} />
+				</Stack>
+			</AppBar>
+
+			<Stack
+				direction='row'
+				sx={{
+					background: 'background.paper',
+					marginTop: `${LOGIN_HEADER_HEIGHT}px`,
+					width: '100%',
+					height: `calc(100vh - ${LOGIN_HEADER_HEIGHT}px)`,
+					overflow: 'hidden',
+					position: 'relative',
+					display: 'flex',
+					alignItems: 'center',
+				}}
+			>
+				<BackgroundImage />
+
+				<Paper
+					sx={{
+						width: 0.95,
+						height: 'auto',
+						maxWidth: 350,
+						boxShadow: 3,
+						borderRadius: 3,
+						marginInline: 'auto 15%',
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						flexWrap: 'nowrap',
+						alignItems: 'center',
+						position: 'relative',
+						overflow: 'hidden',
+						padding: 4,
+						gap: 4,
+						'& > form': {
+							'& > .MuiGrid-root:first-of-type': {
+								gap: 3,
+							},
+							'& > .MuiGrid-root > .MuiButton-root': {
+								width: 1,
+								height: 50,
+								marginTop: 1,
+							},
+						},
+					}}
+				>
+					<Typography
+						variant='h1'
+						color='primary'
+					>
+						Login
+					</Typography>
+
+					<SchemaForm
+						schema={loginSchema}
+						isUpdate={false}
+						onSubmit={async (data) => {
+							const user = await login(data);
+							setSetting('user', user);
+							setTimeout(() => {
+								navigate('/');
+							}, 500);
+							return 'login successful! redirecting...';
+						}}
+					/>
+
+					<WiMetrixLogo
+						width={150}
+						showPoweredBy
+					/>
+				</Paper>
+			</Stack>
+		</>
+	);
+};
+
+Login.loader = loader;
---
+++ src/app/routes/login/login.test.tsx
@@ -0,0 +1,89 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import { renderWithProviders } from '~/shared/helpers/test';
+
+import { Login } from './login.route';
+
+type SetupParams = {
+	username?: string;
+	password?: string;
+	shouldClick?: boolean;
+};
+
+const setup = async (
+	{ username, password, shouldClick }: SetupParams = {
+		username: '',
+		password: '',
+	}
+) => {
+	const user = userEvent.setup();
+
+	renderWithProviders(<Login />);
+
+	const userInput = screen.getByLabelText(/user name/iu);
+	const passwordInput = screen.getByLabelText(/password/iu);
+	const submitButton = screen.getByRole('button', {
+		name: /login/iu,
+	});
+
+	if (username) await user.type(userInput, username);
+	else await user.clear(userInput);
+
+	if (password) await user.type(passwordInput, password);
+	else await user.clear(userInput);
+
+	if (shouldClick) await user.click(submitButton);
+
+	return {
+		submitButton,
+	};
+};
+
+test('disables submission when user or password is empty', async () => {
+	const { submitButton } = await setup();
+	expect(submitButton).toBeDisabled();
+});
+
+test('enables submission when user and password are entered', async () => {
+	const { submitButton } = await setup({
+		username: 'test',
+		password: 'test',
+	});
+
+	expect(submitButton).toBeEnabled();
+});
+
+test.skip('logs user into the app on correct login', async () => {
+	await setup({
+		username: 'test',
+		password: 'test',
+		shouldClick: true,
+	});
+
+	const status = await screen.findByText(/login successful/iu);
+	expect(status).toBeInTheDocument();
+});
+
+test.skip('shows appropriate error when user is incorrect', async () => {
+	await setup({
+		username: 'test1',
+		password: 'test',
+		shouldClick: true,
+	});
+
+	const status = await screen.findByText(/no user with this username exists/iu);
+	expect(status).toBeInTheDocument();
+});
+
+test.skip('shows appropriate error when password is incorrect', async () => {
+	await setup({
+		username: 'test',
+		password: 'test1',
+		shouldClick: true,
+	});
+
+	const status = await screen.findByText(/incorrect signin information/iu);
+	expect(status).toBeInTheDocument();
+});
