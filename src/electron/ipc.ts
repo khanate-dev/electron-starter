5378ad7fda261ab06a13f75d84737d74feb9aca1
SuperKXT <superkxt@outlook.com>
2023-05-27 17:16:17 +0500

    Move `serial-port` ipc into main ipc folder

--- src/electron/ipc.ts
+++ src/electron/ipc.ts
@@ -1,10 +1,55 @@
 import { app, ipcMain } from 'electron';
+import os from 'os';
 
-import { setupSerialPorts } from './serial-port';
+import { SerialPort } from 'serialport';
 
 import type { BrowserWindow } from 'electron';
+import type { IpcApi } from '~/shared/types/ipc';
+
+const paths: Partial<Record<NodeJS.Platform, string>> & { default: string } = {
+	win32: 'COM102',
+	linux: '/dev/ttyUSB0',
+	default: '/dev/ttyUSB0',
+};
 
 export const setupIpc = (mainWindow: BrowserWindow) => {
 	ipcMain.on('closeApplication', () => app.exit());
-	setupSerialPorts(mainWindow);
+
+	const reader = new SerialPort({
+		path: paths[os.platform()] ?? paths.default,
+		autoOpen: false,
+		baudRate: 9600,
+	});
+
+	const connectBarCodeReader: IpcApi['barCodeReader']['connect'] = async () => {
+		if (reader.isPaused()) {
+			reader.resume();
+			return;
+		}
+		if (reader.isOpen || reader.opening) return;
+		return new Promise<void>((resolve, reject) => {
+			reader.open((error) => {
+				error ? reject(error) : resolve();
+			});
+		});
+	};
+
+	const disconnectBarCodeReader: IpcApi['barCodeReader']['disconnect'] =
+		async () => {
+			if (!reader.isOpen || reader.closing) return;
+			return new Promise<void>((resolve, reject) => {
+				reader.close((error) => {
+					error ? reject(error) : resolve();
+				});
+			});
+		};
+
+	ipcMain.handle('connectBarCodeReader', connectBarCodeReader);
+	ipcMain.handle('disconnectBarCodeReader', disconnectBarCodeReader);
+
+	reader.on('data', (data) => {
+		const dataString = data.toString('utf-8');
+		const newBarCodeScan = parseInt(dataString);
+		mainWindow.webContents.send('onRead', newBarCodeScan);
+	});
 };
