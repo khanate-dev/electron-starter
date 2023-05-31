c8e32d2955976734d3962e945fb3dfc124ca85a2
SuperKXT <superkxt@outlook.com>
2023-05-31 11:16:55 +0500

    Add `electron.config.ts`

---
+++ src/electron/electron.config.ts
@@ -0,0 +1,11 @@
+export const electronConfig = {
+	id: 'cbl-garment-tracking',
+	name: 'CBL Garment Tracking',
+	description: 'Garment Tracking Desktop Application For CBL',
+	icoIcon: './src/app/assets/favicon/icon.ico',
+	pngIcon: './src/app/assets/favicon.png',
+	repository: {
+		owner: 'WiMetrixDev',
+		name: 'cbl-garment-tracking',
+	},
+};
