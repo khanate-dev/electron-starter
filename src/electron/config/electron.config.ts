13526bc81a9931efa3a36ace53076d2ccaef178d
SuperKXT <superkxt@outlook.com>
2023-05-31 15:11:59 +0500

    Fix icon paths in `electron.config.ts`

--- src/electron/config/electron.config.ts
+++ src/electron/config/electron.config.ts
@@ -2,10 +2,11 @@ export const electronConfig = {
 	id: 'cbl-garment-tracking',
 	name: 'CBL Garment Tracking',
 	description: 'Garment Tracking Desktop Application For CBL',
-	icoIcon: './src/app/assets/favicon/icon.ico',
-	pngIcon: './src/app/assets/favicon.png',
+	icnsIcon: './src/app/assets/favicon/icons/mac/icns.ico',
+	icoIcon: './src/app/assets/favicon/icons/win/icon.ico',
+	pngIcon: './src/app/assets/favicon/favicon.png',
 	repository: {
 		owner: 'WiMetrixDev',
 		name: 'cbl-garment-tracking',
 	},
-};
+} as const;
