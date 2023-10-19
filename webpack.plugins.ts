8abaf6987791fbe2c05b273611274042ed2ac71b
SuperKXT <superkxt@outlook.com>
2023-10-19 14:23:08 +0500

    Fix type error in `webpack.plugins.ts`

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires, import/no-commonjs */
 import type TReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import type TForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import type { Configuration } from 'webpack';
 
 const ForkTsCheckerWebpackPlugin =
 	require('fork-ts-checker-webpack-plugin') as typeof TForkTsCheckerWebpackPlugin;
 const ReactRefreshWebpackPlugin =
 	require('@pmmmwh/react-refresh-webpack-plugin') as typeof TReactRefreshWebpackPlugin;
 
-export const plugins: (
-	| TForkTsCheckerWebpackPlugin
-	| TReactRefreshWebpackPlugin
-)[] = [new ForkTsCheckerWebpackPlugin({ logger: 'webpack-infrastructure' })];
+export const plugins: NonNullable<Configuration['plugins']> = [
+	new ForkTsCheckerWebpackPlugin({ logger: 'webpack-infrastructure' }) as never,
+];
 
-if (process.env.DEV) plugins.push(new ReactRefreshWebpackPlugin());
+if (process.env.DEV) plugins.push(new ReactRefreshWebpackPlugin() as never);
