f55d86791c8f1c4170a21abacc9f9fcea9236176
SuperKXT <superkxt@outlook.com>
2023-10-28 18:24:56 +0500

    Overhaul `serial-port.ts`

--- src/electron/serial-port.ts
+++ src/electron/serial-port.ts
@@ -14,41 +14,67 @@ const port = new SerialPort({
 	baudRate: 9600,
 });
 
-export const codeReader = {
-	connect: async () => {
-		return new Promise<void>((resolve, reject) => {
-			if (port.isPaused()) {
-				port.resume();
-				resolve();
-				return;
-			}
-			if (port.isOpen || port.opening) {
-				resolve();
-				return;
-			}
-			port.open((error) => {
-				error ? reject(error) : resolve();
-			});
+export type SerialPortListener = (
+	data:
+		| { type: 'connected' | 'disconnected' | 'paused' | 'resumed' }
+		| { type: 'data'; value: string }
+		| { type: 'error'; error: Error },
+) => void;
+
+const listeners: Array<SerialPortListener> = [];
+
+port.on('data', (data) => {
+	const dataString = (data as { toString(encoding: string): string }).toString(
+		'utf-8',
+	);
+	for (const cb of listeners) cb({ type: 'data', value: dataString });
+});
+
+port.on('close', () => {
+	for (const cb of listeners) cb({ type: 'disconnected' });
+});
+
+port.on('open', () => {
+	for (const cb of listeners) cb({ type: 'connected' });
+});
+
+port.on('error', (error) => {
+	for (const cb of listeners) cb({ type: 'error', error });
+});
+
+port.on('pause', () => {
+	for (const cb of listeners) cb({ type: 'paused' });
+});
+
+port.on('resume', () => {
+	for (const cb of listeners) cb({ type: 'resumed' });
+});
+
+export const serialPort = {
+	connect: () => {
+		if (port.isOpen || port.opening) return;
+		port.open();
+	},
+	disconnect: () => {
+		if (!port.isOpen || port.closing) return;
+		port.close();
+	},
+	resume: () => {
+		if (!port.isPaused()) return;
+		port.resume();
+	},
+	pause: () => {
+		if (port.isPaused()) return;
+		port.pause();
+	},
+	status: async () => {
+		return new Promise<'paused' | 'connected' | 'disconnected'>((resolve) => {
+			resolve(
+				port.isPaused() ? 'paused' : port.isOpen ? 'connected' : 'disconnected',
+			);
 		});
 	},
-	disconnect: async () => {
-		return new Promise<void>((resolve, reject) => {
-			if (!port.isOpen || port.closing) {
-				resolve();
-				return;
-			}
-			port.close((error) => {
-				error ? reject(error) : resolve();
-			});
-		});
-	},
-	listen: (callback: (data: number) => void) => {
-		port.on('data', (data) => {
-			const dataString = (
-				data as { toString(encoding: string): string }
-			).toString('utf-8');
-			const parsed = parseInt(dataString);
-			callback(parsed);
-		});
+	listen: (callback: SerialPortListener) => {
+		listeners.push(callback);
 	},
 };
